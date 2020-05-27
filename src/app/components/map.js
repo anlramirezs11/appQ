92eac652a4022186cd9207d7771483d7105fc2f6
Andrea Ramirez <anlramirezs11@gmail.com>
2020-05-27 16:35:37 -0500

    Se logra integrar la libreria de react-native-maps

--- src/app/components/map.js
+++ src/app/components/map.js
@@ -1,42 +1,45 @@
-import React, { Component } from 'react';
-import {
-    View,
-    StyleSheet,
-    Text
-} from 'react-native';
+import React, {Component} from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
 class Map extends Component {
-    render() {
-        return (
-            <View>
-                <Text style={styles.title}>Mapa de Bogotá</Text>
-                <View style={styles.map}>
-                    <Text>asdafaf</Text>
-                </View>
-            </View>
-        );
-    }
-
-
+  render() {
+    return (
+      <View>
+        <Text style={styles.title}>Mapa de Bogotá</Text>
+        <View style={styles.map}>
+          <MapView
+            style={styles.map}
+            region={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.015,
+              longitudeDelta: 0.0121,
+            }}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    map: {
-        width: '100%',
-        height: 270,
-        backgroundColor: '#ff0000'
-    },
-    title:{
-        textAlign: 'center' ,
-        fontWeight: 'bold',
-        fontSize: 28,
-        color: '#ffffff',
-        marginTop: 50,
-        marginBottom: 35,
-        fontFamily: 'Roboto',
-        letterSpacing: 3,
-        textTransform: 'uppercase'
-    }
+  map: {
+    width: '100%',
+    height: 270,
+    backgroundColor: '#ff0000',
+  },
+  title: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 28,
+    color: '#ffffff',
+    marginTop: 50,
+    marginBottom: 35,
+    fontFamily: 'Roboto',
+    letterSpacing: 3,
+    textTransform: 'uppercase',
+  },
 });
 
 export default Map;
