2eb85b6a4050390d570e62a6393a0ad61e85a598
Andrea Ramirez <anlramirezs11@gmail.com>
2020-06-01 21:05:39 -0500

    Se crea la vista para registrarse

--- src/app/app.js
+++ src/app/app.js
@@ -2,13 +2,14 @@ import React, {Component} from 'react';
 //Containers
 import Main from './containers/main';
 import Login from './containers/login';
+import SingUp from './containers/singUp';
 
 
 class AppLayout extends Component {
   render() {
     return (
       // <Main />
-      <Login />
+      <SingUp />
     );
   }
 }
